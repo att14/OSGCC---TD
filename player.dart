a3830bf16f53db87744e16b2b12b8f0dfaa8f133
Sean Myers <seanmyers0608@gmail.com>
2011-11-05 19:47:11 -0400

    newest commit

--- player.dart
+++ player.dart
@@ -1,9 +1,31 @@
 class Player {
-  int life, money;
+  int _life, _money;
   
-  Player(this.life, this.money);
-  
-  bool canBuy(int cost)=> (life-cost > 0); 
-  
+  Player(this._life, this._money);
+
+  //getters + setters for life
+  int get life() => _life;
+  void set life(newLife){
+    _life = newLife;
+  }
+///////////////////////
+
+
+  void set money(newMoney){
+    if(newMoney >= 0){
+         _money = newMoney;   
+    }
+   }
+  int get money()=> _money;
+
   bool gameOver() => life < 0;
- } 
+
+//Returns false if you do not have enough money
+  bool buyTower(int cost){
+    if(life-cost >= 0){
+      this._money -= cost;
+      return true;
+    }
+    return false;
+  }
+ }
