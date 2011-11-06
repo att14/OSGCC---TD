16110a9b058140992111a6df2c2488ca04570780
Sean Myers <seanmyers0608@gmail.com>
2011-11-05 23:20:49 -0400

    newest commit

--- td.dart
+++ td.dart
@@ -5,11 +5,14 @@
 #resource('index.html');
 
 class td {
-
+  List<Tower> towers;
   td() {
     
   }
-
+  void onMouseMove(){
+    //HTMLCanvasElement can =document.getElementById('canvas');
+    
+  }
   void run() {
     write("Hello World!");
   }
@@ -24,9 +27,15 @@ class td {
 }
 
 void main() {
-  new Tower(100, 105, 100).placeTower(150,150);
-  new Tower(100, 105, 100).placeTower(10,10);
-  new Tower(100, 105, 100).placeTower(50,50);
-
+  Tower c = new Tower(100, 105, 100);
+  c.placeTower(150,150);
+  Tower a = new Tower(100, 105, 100);
+  a.placeTower(10,10);
+  Tower b = new Tower(100, 105, 100);
+  b.placeTower(50,50);
+  if(a.intersect(b.x, b.y)){
+  }
+  new td().write("${a.intersect(b.x, b.y)}");
+  new td().write("${a.intersect(c.x, c.y)}");
 
 }
--- tower.dart
+++ tower.dart
@@ -3,35 +3,62 @@ class Tower {
   HTMLCanvasElement canvas;
   CanvasRenderingContext2D ctx;
   int sight, hit, cost, x, y;
-  bool drawable;
+  bool drawable, over;
   static final int TAU = 2*Math.PI;
   Tower(this.cost, this.sight, this.hit){
     HTMLDocument doc = window.document;
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext("2d");
+    addEventHandlers();
     drawable = false;
   }
-  void drawTower(){
-    drawTowerOver();
-    ctx.beginPath();
-    ctx.setLineWidth(2);
-    ctx.setFillColor("orange");
-    ctx.setStrokeColor("orange");
-    ctx.arc(x, y, 5, 0, TAU, false);
-    ctx.fill();
-    ctx.closePath();
-    ctx.stroke();
+  
+  void addEventHandlers(){
+    canvas.addEventListener("mousemove", this.onMouseMove, true);
+    
   }
-  void drawTowerOver(){
+  void drawTower(){
+    if(drawable){
+      ctx.beginPath();
+      ctx.setLineWidth(2);
+      ctx.setFillColor("orange");
+      ctx.setStrokeColor("orange");
+      ctx.arc(x, y, 5, 0, TAU, false);
+      ctx.fill();
+      ctx.closePath();
+      ctx.stroke();      
+    }
+
+  }
+  void onMouseMove(MouseEvent e){
+    int mouseX = e.offsetX;
+    int mouseY = e.offsetY;
+    if(this.intersect(mouseX, mouseY)){
+      this.drawTowerOver("rgb(255,255,255)","rgb(255,255,255)");
+      this.drawTowerOver("rgba(192,202,85,.9)", "rgb(0,0,0)");
+      this.drawTower();
+
+    }
+    else{
+      this.drawTowerOver("rgb(255,255,255)", "rgb(255,255,255)");
+      this.drawTower();      
+    }
+    HTMLDocument doc = window.document;
+    HTMLDivElement a = doc.getElementById("mouseLoc");
+    a.innerHTML = "$mouseX, $mouseY, $x, $y";
+  }
+  void drawTowerOver(String color, String color2){
     ctx.beginPath();
     ctx.setLineWidth(2);
-    ctx.setFillColor("rgba(51,102,255,.2)");
-    ctx.setStrokeColor("rgb(61,0,245)");
+    ctx.setFillColor(color);
+    ctx.setStrokeColor(color2);
     ctx.arc(x, y, sight, 0, TAU, false);
     ctx.fill();
     ctx.closePath();
     ctx.stroke();    
   }
+  bool intersect(int x_i, int y_i) => ((x_i-x)*(x_i-x) + (y_i-y)*(y_i-y)-sight*sight) <= 0;
+  
   void placeTower(int ix,int iy){
     x = ix;
     y = iy;
