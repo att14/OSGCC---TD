d4666edd3c96fdfe401a685d9d45b72b456ede0d
Andrew Tribone <att14@pitt.edu>
2011-11-06 03:24:53 -0500

    background is there, and they movegit statusgit status

--- enemy.dart
+++ enemy.dart
@@ -8,27 +8,39 @@ class Enemy {
   int health;
   int size;
   bool hit;
+  HTMLImageElement img;
   HTMLCanvasElement canvas;
   CanvasRenderingContext2D ctx;
   
   Enemy (this.x, this.y, this.sight, this.speed, this.damage, this.health, this.size){
-    castle = (Constants.SIZE *.8).ceil();
+    castle = (750 *.73).floor();
     hit = false;
     HTMLDocument doc = window.document;
-    canvas = document.getElementById('canvas');
+    canvas = doc.getElementById('canvas');
     ctx = canvas.getContext('2d');
+    
+    img = doc.createElement('img');
+    img.src = 'images/monsters/blob.png';
+    img.onload = (e) {
+      ctx.drawImage(img, x, y);
+    };
   }
   
   bool move() {
-    double distance = Math.sqrt(Math.pow(y - castle, 2));
-    if (distance < castle) {
+    double distance = Math.sqrt(Math.pow(x - castle, 2));
+    if (distance < sight) {
       hit = true;
     }
     
     if (hit == true) {
       return false;
     }else {
-      y += speed;
+      x += speed;
+      if (x + size >= castle) {
+        x = castle - size;
+      }
+      print('x: $x');
+      print('distance: $distance');
       return true;
     }
   }
@@ -44,7 +56,6 @@ class Enemy {
   int get life() => health;
   
   void draw() {
-    ctx.setFillColor('orange');
-    ctx.fillRect(x, y, size, size);
+    ctx.drawImage(img, x, y);
   }
 }
